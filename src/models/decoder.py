bb43d108b5d61c7d1c8b88380bb1512192c3ef55
Ankush Ganguly <ankushganguly13@gmail.com>
2018-11-25 11:34:00 +0700

    Decoder modified to dynamically perform deconvolutions based on output shape

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -3,8 +3,9 @@ from keras.layers import Convolution2D, UpSampling2D, Deconvolution2D, Reshape, 
 
 
 class DeepMindDecoder:
-    def __init__(self, decoder_input):
+    def __init__(self, decoder_input, output_shape):
         self._dec_input = decoder_input
+        self.__dewconv_func_reps = int(output_shape / 16)
 
     @staticmethod
     def deconv_func(input_layer):
@@ -19,7 +20,7 @@ class DeepMindDecoder:
 
         x = UpSampling2D((4, 4))(x)
 
-        for _ in range(4):
+        for _ in range(self.__dewconv_func_reps):
             x = self.deconv_func(x)
 
         x = Convolution2D(1, (1, 1), padding='same', activation='sigmoid')(x)
