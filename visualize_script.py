9afa7d70cb02f19d03f47f660e2a6b77ea15cc72
ankush <agang@sertiscorp.com>
2018-11-28 18:09:42 +0700

    Visaulizing script refactored

--- visualize_script.py
+++ visualize_script.py
@@ -3,10 +3,36 @@ import imageio
 import os
 import argparse
 import sys
+import numpy as np
+from src.common import utils
+from PIL import Image
 
 
 def main(args):
-    pass
+    test_image = Image.open(args.test_image)
+    enc_inp = test_image.resize((args.image_size, args.image_size), Image.NEAREST)
+    enc_inp = np.array([np.asarray(enc_inp) / 255.])
+    enc_inp = utils.pre_process_input(enc_inp)
+
+    enc_out = enc_model.predict(enc_inp)
+
+    traversal_range = np.arange(args.start_range, args, 1/float(args.traversal_steps))
+
+    dec_inp = list(enc_out.ravel())
+
+    print('Generating phase....')
+    for emb_dim in range(len(dec_inp)):
+        print('Traversing embedding dimension: {}'.format(emb_dim + 1))
+        images = []
+        inp = dec_inp
+        for val in list(traversal_range):
+            inp[emb_dim] = val
+            inp_arr = np.array(inp)
+            inp_arr = inp_arr.reshape((-1, len(traversal_range)))
+            dec_out = dec_model.predict(inp_arr)
+            images.append(dec_out[0])
+        name = 'z_' + str(emb_dim+1) + '.gif'
+        imageio.mimsave(name, images)
 
 
 def parse_arguments(argv):
